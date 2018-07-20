ee28cdca3f2dce7ca7321b9b8c8f22a768d77247
jerry <jerry@ThinkPad-X230>
2018-07-20 11:18:20 +0800

    迁移点评抓取

---
+++ site-dianping/src/main/java/com/edmi/site/dianping/entity/Version.java
@@ -0,0 +1,9 @@
+package com.edmi.site.dianping.entity;
+
+public class Version {
+	
+	public static final String version = "init";
+	
+	public static final String version_last_month = "201806";
+	
+}
