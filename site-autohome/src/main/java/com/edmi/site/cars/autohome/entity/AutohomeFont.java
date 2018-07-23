7706ee109676caca52f5b2aeb3d70d9605ddc30a
keshi <keshi.wang@earlydata.com>
2018-07-23 11:02:42 +0800

    # WARNING: head commit changed in the meantime
    汽车之家口碑解析

--- site-autohome/src/main/java/com/edmi/site/cars/autohome/entity/AutohomeFont.java
+++ site-autohome/src/main/java/com/edmi/site/cars/autohome/entity/AutohomeFont.java
@@ -1,27 +1,74 @@
 package com.edmi.site.cars.autohome.entity;
 
+import java.util.Date;
+
+import org.apache.commons.lang3.time.DateFormatUtils;
+
 import fun.jerry.entity.annotation.ColumnMapping;
 import fun.jerry.entity.annotation.TableMapping;
 
 @TableMapping("autohome_font")
 public class AutohomeFont {
+	@ColumnMapping("autohomeHtml")
+	private String autohomeHtml;
+	
+	@ColumnMapping("autohomeChar")
+	private String autohomeChar;
+
 	@ColumnMapping("hex_value")
 	private String hexValue;
-	
+
 	@ColumnMapping("x_value")
 	private String XValue;
-	
+
 	@ColumnMapping("y_value")
 	private String YValue;
-	
+
 	@ColumnMapping("width")
 	private String width;
-	
+
 	@ColumnMapping("height")
 	private String height;
-	
+
 	@ColumnMapping("font_name")
 	private String fontName;
+
+	private String InsertTime = DateFormatUtils.format(new Date(), "yyyy-MM-dd HH:mm:ss");
+
+	private String UpdateTime = DateFormatUtils.format(new Date(), "yyyy-MM-dd HH:mm:ss");
+
+	
+	public String getAutohomeHtml() {
+		return autohomeHtml;
+	}
+
+	public void setAutohomeHtml(String autohomeHtml) {
+		this.autohomeHtml = autohomeHtml;
+	}
+
+	public String getAutohomeChar() {
+		return autohomeChar;
+	}
+
+	public void setAutohomeChar(String autohomeChar) {
+		this.autohomeChar = autohomeChar;
+	}
+
+	public String getInsertTime() {
+		return InsertTime;
+	}
+
+	public void setInsertTime(String insertTime) {
+		InsertTime = insertTime;
+	}
+
+	public String getUpdateTime() {
+		return UpdateTime;
+	}
+
+	public void setUpdateTime(String updateTime) {
+		UpdateTime = updateTime;
+	}
 
 	public String getHexValue() {
 		return hexValue;
--- site-autohome/src/main/java/com/edmi/site/cars/autohome/parse/AutohomeFontParser.java
+++ site-autohome/src/main/java/com/edmi/site/cars/autohome/parse/AutohomeFontParser.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Scanner;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -34,26 +35,32 @@ import fun.jerry.entity.system.SqlEntity;
 import fun.jerry.entity.system.SqlType;
 
 public class AutohomeFontParser {
-	
+
 	private static Logger log = LoggerFactory.getLogger(AutohomeFontParser.class);
 
-	public static void parseMobileFont (String detail) {
+	public static void parseMobileFont(String detail, String htmlFileName) {
 		TTFParser ttf = new TTFParser();
-//		String fontData = HtmlDataUtil.getPathDatas(new File("D:/autohome_font/autohomefontmap.txt"));
-//		JSONObject fontJson = JSON.parseObject(fontData);
+		// String fontData = HtmlDataUtil.getPathDatas(new
+		// File("D:/autohome_font/autohomefontmap.txt"));
+		// JSONObject fontJson = JSON.parseObject(fontData);
+		int tatal = 0;
+		int countOneFile = 0;
 		try {
 			String fontUrl = StringHelper.getResultByReg(detail, "url\\('(//k[1-9]{1}.autoimg.cn[^\\.]+..ttf)");
-			String path = HtmlDataUtil.saveFontData(fontUrl,"d:/autohome_font/font");
+			String path = HtmlDataUtil.saveFontData(fontUrl, "E:\\data\\program\\autohome\\autohome_font\\font_test");
 			TrueTypeFont font = ttf.parse(path);
-//			log.info(font.getTableMap().values());
-			
-		//	String detail = WebDriverSupport.load(driver, reputationInfo.getReputationUrl());
-//			detail = org.apache.commons.lang.StringEscapeUtils.escapeHtml(detail);
-			HtmlDataUtil.saveData("d:/escapeautodata1.txt", detail);
-//			List<String> encodes = StringHelper.getResultListByReg(detail, "font-family:myfont;&quot;&gt;(&#[\\d]+;)&lt");
-//			List<String> encodes = StringHelper.getResultListByReg(detail, "font-family:myfont'>(&#[\\d]+;)<");
-			
-			String regex="&#x[A-Fa-f0-9]{4};";
+			// log.info(font.getTableMap().values());
+
+			// String detail = WebDriverSupport.load(driver,
+			// reputationInfo.getReputationUrl());
+			// detail = org.apache.commons.lang.StringEscapeUtils.escapeHtml(detail);
+			HtmlDataUtil.saveData("E:/escapeautodata1.txt", detail);
+			// List<String> encodes = StringHelper.getResultListByReg(detail,
+			// "font-family:myfont;&quot;&gt;(&#[\\d]+;)&lt");
+			// List<String> encodes = StringHelper.getResultListByReg(detail,
+			// "font-family:myfont'>(&#[\\d]+;)<");
+
+			String regex = "&#x[A-Fa-f0-9]{4};";
 			Pattern pattern = Pattern.compile(regex);
 			Matcher matcher = pattern.matcher(detail);
 			List<String> encodes = Lists.newArrayList();
@@ -65,64 +72,90 @@ public class AutohomeFontParser {
 					encodes.add(string.replace("&#x", "").replace(";", "").toUpperCase());
 				}
 			}
-			
+			Scanner sc = new Scanner(System.in);
+			HashSet<String> codeset = new HashSet<>();
+			System.out.println("encodes.size:" + encodes.size());
+
 			for (String codeu : encodes) {
-				
 				AutohomeFont af = new AutohomeFont();
-				
-//				System.out.println(encodes.get(i));
-//				String num = StringHelper.getResultByReg(encodes.get(i), "([\\d]+)");
-//				System.out.println(num);
-//				String codeu = Integer.toHexString(Integer.parseInt(num));
-//				String codeu = num + "";
-				System.out.println(font.getPath("uni"+codeu).getCurrentPoint());
-//				String data = fontJson.getString(String.valueOf(font.getPath("uni"+codeu).getCurrentPoint().getY()));
-//				String data = fontJson.getString(String.valueOf(font.getPath("uni"+codeu).getCurrentPoint().getY()));
-//				String data = fontJson.getString(String.valueOf(font.getPath("uni"+codeu).getCurrentPoint()));
+				tatal++;
+				// System.out.println(encodes.get(i));
+				// String num = StringHelper.getResultByReg(encodes.get(i), "([\\d]+)");
+				// System.out.println(num);
+				// String codeu = Integer.toHexString(Integer.parseInt(num));
+				// String codeu = num + "";
+				System.out.println(font.getPath("uni" + codeu).getCurrentPoint());
+				// String data =
+				// fontJson.getString(String.valueOf(font.getPath("uni"+codeu).getCurrentPoint().getY()));
+				// String data =
+				// fontJson.getString(String.valueOf(font.getPath("uni"+codeu).getCurrentPoint().getY()));
+				// String data =
+				// fontJson.getString(String.valueOf(font.getPath("uni"+codeu).getCurrentPoint()));
 				try {
-					String x = String.valueOf(font.getPath("uni"+codeu).getCurrentPoint().getX());
-					String y = String.valueOf(font.getPath("uni"+codeu).getCurrentPoint().getY());
-					String width = String.valueOf(font.getPath("uni"+codeu).getBounds().width);
-					String height = String.valueOf(font.getPath("uni"+codeu).getBounds().height);
-					
+					String x = String.valueOf(font.getPath("uni" + codeu).getCurrentPoint().getX());
+					String y = String.valueOf(font.getPath("uni" + codeu).getCurrentPoint().getY());
+					String width = String.valueOf(font.getPath("uni" + codeu).getBounds().width);
+					String height = String.valueOf(font.getPath("uni" + codeu).getBounds().height);
+					String autohomeChar = "";
+					if (codeset.contains(codeu)) {
+						System.out.println(codeu + "已经存在");
+						continue;
+					}
+					codeset.add(codeu);
+					countOneFile++;
+					System.out.println("codeset:" + codeset.size());
+					System.out.println("=====第:" + countOneFile + " ,请输入>>> " + codeu + "<<<对应的汉字：");
+					autohomeChar = sc.nextLine();
+					System.out.println("autohomeChar:" + autohomeChar);
+
 					af.setHexValue(codeu);
 					af.setXValue(x);
 					af.setYValue(y);
 					af.setWidth(width);
 					af.setHeight(height);
 					af.setFontName(fontUrl);
-					
-					FirstCacheHolder.getInstance().submitFirstCache(new SqlEntity(af, DataSource.DATASOURCE_SGM, SqlType.PARSE_INSERT));
-					
-//					JSONObject fontJson1 = JSON.parseObject(data);
-//					String key = "width="+font.getPath("uni"+codeu).getBounds().width+",height="+font.getPath("uni"+codeu).getBounds().height;
-				//	System.out.println(key);
-//					data = fontJson1.getString(key);
+					af.setAutohomeChar(autohomeChar);
+					af.setAutohomeHtml(htmlFileName);
+
+					FirstCacheHolder.getInstance()
+							.submitFirstCache(new SqlEntity(af, DataSource.DATASOURCE_SGM, SqlType.PARSE_INSERT));
+
+					// JSONObject fontJson1 = JSON.parseObject(data);
+					// String key =
+					// "width="+font.getPath("uni"+codeu).getBounds().width+",height="+font.getPath("uni"+codeu).getBounds().height;
+					// System.out.println(key);
+					// data = fontJson1.getString(key);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
-//				System.out.println(data);
-//				detail = detail.replaceAll(codeu, data);
+				// System.out.println(data);
+				// detail = detail.replaceAll(codeu, data);
 			}
-//			detail = org.apache.commons.lang.StringEscapeUtils.unescapeHtml(detail);
-//			if (detail.contains("您的网络异常，请根据下面步骤提示进行安全验证")) {
-//				driver.findElement(By.className("yidun_intelli-text")).click();
-//			}
-			
+			System.out.println("tatal:" + tatal);
+			System.out.println("countOneFile:" + countOneFile);
+			// detail = org.apache.commons.lang.StringEscapeUtils.unescapeHtml(detail);
+			// if (detail.contains("您的网络异常，请根据下面步骤提示进行安全验证")) {
+			// driver.findElement(By.className("yidun_intelli-text")).click();
+			// }
+
 		} catch (Exception e) {
 		} finally {
 		}
 	}
-	
+
 	public static void main(String[] args) {
-		
+
 		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-		
-		File dir = new File("D:/home/conner/reputation");
+
+		File dir = new File("E:\\data\\program\\autohome\\autohome_html_test");
 		if (dir.isDirectory()) {
 			File[] list = dir.listFiles();
 			Set<String> fontUrlSet = new HashSet<>();
+			int count = 0;
 			for (File file : list) {
+				String htmlFileName = file.getName();
+				count++;
+				System.out.println("=======" + count + "========读到的文件名：" + htmlFileName);
 				String html = "";
 				try {
 					html = FileUtils.readFileToString(file, Charset.forName("GBK"));
@@ -133,13 +166,16 @@ public class AutohomeFontParser {
 					} else {
 						log.error("fontUrl is null");
 					}
-					parseMobileFont(html);
+					parseMobileFont(html, htmlFileName);
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
+				if (count == 100) {
+					break;
+				}
 			}
 			log.info("fontUrlSet size : {}", fontUrlSet.size());
-			
+
 		}
 	}
 }
